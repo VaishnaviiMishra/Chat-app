e12f754196a8ce055bfab323eb4bf79844d0ccd3
VaishnaviMishra <vaishnavi@sekeron.com>
2024-09-08 12:56:20 +0530

    cc

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,13 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
+
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    outDir: 'dist', // This is where Vite outputs the bundled files
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Ensures Vite resolves both .js and .jsx
+  },
+});
